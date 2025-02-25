6742b06497f04b98c65db5e47affa78b9c7ec47e
vishnu-vardhan121 <kottevishnuvardhann@gmail.com>
2025-02-25 11:50:06 +0530

    trial for code push

---
+++ app/login/page.js
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+function Login() {
+  return <div>Login hiiiiiiiiiiiiiii</div>;
+}
+
+export default Login;
---
+++ app/signin/page.js
@@ -0,0 +1,8 @@
+"use strict";
+import React from "react";
+
+function SigninPage(props) {
+  return <>hiiiiiiiii</>;
+}
+
+export default SigninPage;
